d9a343b644235b143cefce38f0e6ca53c5e27f99
rayhm6419 <haoml6419@gmail.com>
2025-04-02 18:22:30 -0700

    Assignment 3

---
+++ src/main/java/com/bookclub/service/impl/MemBookDao.java
@@ -0,0 +1,37 @@
+package com.bookclub.service.impl;
+
+import com.bookclub.model.Book;
+import com.bookclub.service.dao.BookDao;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MemBookDao implements BookDao {
+
+    private List<Book> books;
+
+    public MemBookDao() {
+        books = new ArrayList<>(Arrays.asList(
+          
+        ));
+    }
+    
+    
+
+    @Override
+    public List<Book> list() {
+        return books;
+    }
+
+    @Override
+    public Book find(String isbn) {
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/com/bookclub/web/HomeController.java
+++ src/main/java/com/bookclub/web/HomeController.java
@@ -1,26 +1,40 @@
 package com.bookclub.web;
+
+import com.bookclub.model.Book;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import com.bookclub.service.dao.BookDao;
+import com.bookclub.service.impl.MemBookDao;
 import org.springframework.ui.Model;
 
 @Controller
-@RequestMapping
+@RequestMapping("/monthly-books")
 
 public class HomeController {
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public String getMonthlyBook(@PathVariable("id") String id, Model model) {
+        MemBookDao bookDao = new MemBookDao();
+        Book book = bookDao.find(id);
+        model.addAttribute("book", book);
+        return "monthly-books/view";
+    }
 
     @RequestMapping(method = RequestMethod.GET)
-        public String showHome(Model model){
-            return "index";
-        }
+    public String showHome(Model model) {
+        return "index";
+    }
 
     @RequestMapping(method = RequestMethod.GET, path = "/about")
-        public String showAboutUs(Model model){
-            return "about";
-        }
+    public String showAboutUs(Model model) {
+        return "about";
+    }
 
     @RequestMapping(method = RequestMethod.GET, path = "/contact")
-        public String showContactsUs(Model model){
-            return "contact";
-        }
+    public String showContactsUs(Model model) {
+        return "contact";
+    }
 }
